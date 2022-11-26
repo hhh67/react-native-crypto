e4ed9954beb54edba6fde4f58363f30f95ba0ea7
hhh67 <hhio.67.2020@gmail.com>
2022-11-27 03:24:48 +0900

    env, constの追加

---
+++ src/const.tsx
@@ -0,0 +1,29 @@
+export const Const: {[key: string]: any} = {
+  COINMARKETCAP: {
+    BASE_URL: 'https://sandbox-api.coinmarketcap.com',
+    ENDPOINT: {
+      MAP: '/v1/cryptocurrency/map',
+    }
+  },
+};
+
+/** ログに色をつけて出力 */
+const ConsoleReset = '\u001b[0m';
+export const ConsoleRed = (...log: any[]) => {
+  console.log('\u001b[31m' + log + ConsoleReset);
+};
+export const ConsoleGreen = (...log: any[]) => {
+  console.log('\u001b[32m' + log + ConsoleReset);
+};
+export const ConsoleYellow = (...log: any[]) => {
+  console.log('\u001b[33m' + log + ConsoleReset);
+};
+export const ConsoleBlue = (...log: any[]) => {
+  console.log('\u001b[34m' + log + ConsoleReset);
+};
+export const ConsoleMagenta = (...log: any[]) => {
+  console.log('\u001b[35m' + log + ConsoleReset);
+};
+export const ConsoleCyan = (...log: any[]) => {
+  console.log('\u001b[36m' + log + ConsoleReset);
+};
