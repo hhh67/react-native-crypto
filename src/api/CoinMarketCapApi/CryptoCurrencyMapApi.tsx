59e61130f9d2f64b83d83850d916e24ea57408ac
hhh67 <hhio.67.2020@gmail.com>
2022-11-27 03:25:25 +0900

    CoinMarketCap ID MapのAPIを追加

---
+++ src/api/CoinMarketCapApi/CryptoCurrencyMapApi.tsx
@@ -0,0 +1,26 @@
+import { Const } from '../../const';
+import { Env } from '../../env';
+
+export async function cryptocurrencyMap(
+  limit: number,
+  listing_status?: 'active'|'inactive'|'untracked',
+  start?: number,
+  sort?: 'id'|'cmc_rank',
+) {
+  const api_url = Const.COINMARKETCAP.BASE_URL + Const.COINMARKETCAP.ENDPOINT.MAP
+    + `?limit=${limit.toString()}`
+    + (listing_status !== undefined ? `&listing_status=${listing_status}`: '')
+    + (start !== undefined ? `&start=${start?.toString()}` : '')
+    + (sort !== undefined ? `&sort=${sort}` : '');
+
+  const res = await fetch(api_url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'X-CMC_PRO_API_KEY': Env.COINMARKETCAP_API_KEY,
+    },
+  });
+  
+  const json = await res.json();
+  return json;
+}
